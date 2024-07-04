d2a73f9b8913a62d316aa3bb1641664a7b5f0cbb
lgordon2@gmail.com <lgordon2@gmail.com>
2024-07-04 12:40:59 -0400

    wip playing

--- components.go
+++ components.go
@@ -1,13 +1,13 @@
 package main
 
 const soundCardComponent = `
-<div
-            class="min-w-72 max-w-sm p-4 m-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 grid grid-cols-1 divide-y divide-gray-700">
+<div id="box-1258261093541875723"
+            class="min-w-72 max-w-sm p-2 m-2 bg-white border border-2 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 grid grid-cols-1 divide-y divide-gray-700">
             <div class="flex flex-row">
                 <h5 class="flex-1 mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">WhatsUpImDoug.mp3
                 </h5>
                 <a class="shrink text-sky-500"
-                    href="https://cdn.discordapp.com/soundboard-sounds/1239382282930884608"><svg
+                    href="https://cdn.discordapp.com/soundboard-sounds/1258261093541875723"><svg
                         class="h-8 w-8 text-blue-500" viewBox="0 0 24 24" fill="none" stroke="currentColor"
                         stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
@@ -17,12 +17,12 @@ const soundCardComponent = `
             </div>
 
             <div class="flex flex-row divide-x divide-gray-700">
-                <button class="flex flex-1 items-center justify-center my-2"><svg class="h-8 w-8 text-gray-500"
+                <button onclick="window._playSound('1258261093541875723')" class="flex flex-1 items-center justify-center my-1"><svg class="h-8 w-8 text-gray-500"
                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                         stroke-linejoin="round">
                         <polygon points="5 3 19 12 5 21 5 3" />
                     </svg></button>
-                <button class="flex flex-1 items-center justify-center my-2"><svg class="h-8 w-8 text-red-500"
+                <button class="flex flex-1 items-center justify-center my-1"><svg class="h-8 w-8 text-red-500"
                         width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
                         stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" />
