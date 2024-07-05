93077627ce3d88814e2b7aafb8499573ed64fe92
lgordon2@gmail.com <lgordon2@gmail.com>
2024-07-05 19:04:13 -0400

    Revert "Revert "minify html""
    This reverts commit e94b83011e3853150656821ae3d047bd295b365c.

--- main.go
+++ main.go
@@ -17,6 +17,9 @@ import (
 	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/tdewolff/minify"
+	"github.com/tdewolff/minify/html"
+	_ "github.com/tdewolff/minify/v2/html"
 )
 
 type DiscordMessage struct {
@@ -89,6 +92,9 @@ func fetchStoredSounds() ([]string, map[string][]byte, error) {
 }
 
 func main() {
+	m := minify.New()
+	m.AddFunc("text/html", html.Minify)
+
 	var userIsInChannel atomic.Bool
 	userIsInChannel.Store(false)
 	var mu sync.RWMutex
@@ -140,7 +146,9 @@ func main() {
 		}
 		buf.WriteString("</div>")
 		buf.WriteString("</div>")
-		return buf
+		var minifiedBuf bytes.Buffer
+		m.Minify("text/html", &minifiedBuf, &buf)
+		return minifiedBuf
 	}
 	go func() {
 		for range soundUpdates {
