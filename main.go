32b51a54d316110b66e16f489f55e4a500f14cf4
Lew Gordon <lgordon2@gmail.com>
2024-07-08 20:58:59 -0400

    increase buffer size

--- main.go
+++ main.go
@@ -48,8 +48,8 @@ const channelID = "284709094588284930" // general channel
 // const channelID = "752332599631806509" // general channel
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize:  20 * 1024,
-	WriteBufferSize: 20 * 1024,
+	ReadBufferSize:  32 * 1024,
+	WriteBufferSize: 32 * 1024,
 } // use default options
 
 func deleteButton(soundId, guildId, username, avatarCDN string, disabled bool) string {
